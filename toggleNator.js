6516694a65245644895ac5f707cd222321bceb2d
Tom Kopp <witzbould@live.com>
2015-11-01 23:50:49 +0100

    add umd functionality and module foundation

--- toggleNator.js
+++ toggleNator.js
@@ -7,16 +7,54 @@
  * @license MIT
  * @version 2.x.x
  */
-;(function (toggleNator) {
-	
-	 function _privateAdder(n1, n2) {
-        return n1 + n2;
+; (function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.toggleNator = root.toggleNator || factory();
     }
- 
-    toggleNator.add = function(n1, n2) {
-        return _privateAdder(n1);
-    };
-    
-    console.log(this, toggleNator)
-	
-})(window.toggleNator = window.toggleNator || {})
+} (this, function toggleNatorFactory() {
+
+    var _groups = {}
+
+    function toggleNator(elements, options) {
+        this.elements = elements
+        this.options = options
+
+        _init(this)
+    }
+
+    function _init(that) {
+        // overwrite defaults with given options
+        // prepare elements
+        // bind new eventlistener to document
+        // apply once to initialize element states
+    }
+
+    function _write(param) {
+        console.log(param)
+    }
+
+    toggleNator.prototype = {
+        defaults: {
+            byGroup: true
+            , group: 'global'
+            , state: 'off'
+            , triggerClass: 'toggleNator'
+            , target: null
+            , targetClass: 'toggleNatorTarget'
+        }
+
+        , write: function (param) { _write(param) }
+    }
+
+
+    return toggleNator;
+}));
