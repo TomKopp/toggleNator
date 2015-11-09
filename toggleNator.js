9b53467892f3b5381d2516cae7d9ecfd51cc142e
Tom Kopp <tom.kopp@eos-uptrade.de>
2015-11-09 10:06:28 +0100

    add prepareElements with some helper functionality

--- toggleNator.js
+++ toggleNator.js
@@ -1,6 +1,6 @@
 /**
  * toggleNator.js
- * 
+ *
  * This version is an advancement and does not need jQuery anymore.
  *
  * @author Tom Kopp
@@ -22,20 +22,48 @@
     }
 } (this, function toggleNatorFactory() {
 
-    var _groups = {}
-
-    function toggleNator(elements, options) {
-        this.elements = elements
-        this.options = options
+    function toggleNator(triggers, options) {
+        this.triggers = triggers || null
+        this.options = options || null
+        this.groups = {}
 
         _init(this)
+        _prepareElements(this)
     }
 
-    function _init(that) {
-        // overwrite defaults with given options
+    function _init(nator) {
+        // overwrite global defaults with given options
         // prepare elements
         // bind new eventlistener to document
         // apply once to initialize element states
+    }
+
+    function _prepareElements(nator) {
+        // list of trigger
+        var myNodeList = nator.triggers
+        if (myNodeList === null) {
+            return
+        }
+        for (var i = 0, lgth = myNodeList.length; i < lgth; ++i) {
+            var item = myNodeList[i]
+            item.data = _data(item, 'toggleNator')
+            // override globally adjusted defaults with element options
+
+            // get target element and save it for later use
+            item.target = item.data.target
+            // add element to appropriate group
+
+            _write(item.target)
+        }
+    }
+
+    function _data(element, name, value) {
+        if (value === undefined) {
+            value = element.getAttribute('data-' + name)
+            return JSON.parse(value)
+        } else {
+            element.setAttribute('data-' + name, JSON.stringify(value))
+        }
     }
 
     function _write(param) {
